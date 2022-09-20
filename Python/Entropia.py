0bb654af15a72ecd4360e39701aaeb91060eba64
unjugador16 <bermudeztomas06@gmail.com>
2022-09-19 20:57:55 -0500

    some random thing

---
+++ Python/Entropia.py
@@ -0,0 +1,18 @@
+from random import randint
+
+particulas = int(input('Introduzca el número de particulas del sistema: '))
+
+tipos_part = int(input('Intoduzca la cantidad de estados de energia que tiene el sistema: '))
+
+Energia = input('Introduzca la cantidad de energia del sistema (Si no tiene ingrese "n"): ')
+
+macroestados = []
+
+while True:
+    break
+for i in range(0,particulas + 1):
+    macroestados.append(list())
+    r = particulas - i
+#       macroestados.append()
+
+print(macroestados)
--- Python/Pruebas.py
+++ Python/Pruebas.py
@@ -1,3 +1,14 @@
-import random
+from random import randint
+var1 = 0
+var2 = 0
+var3 = 0
+for _ in range(1,100):
+    i = randint(1,3)
+    if i == 1:
+        var1 += 1
+    if i == 2:
+        var2 += 1
+    if i == 3:
+        var3 += 1
 
-print(random.random())
+print(var1, var2 , var3)
